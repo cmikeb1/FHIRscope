2700bab84b1237f2f6dd34fafd55881dbd11d5f5
C. Mike Bylund <c.mike.bylund@imail.org>
2016-08-16 13:00:53 -0600

    Updated angular to rc.5

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,14 +2,20 @@ import {Routes, RouterModule} from "@angular/router";
 import {SelectorStaticListComponent} from "./selector-static-list/selector-static-list.component";
 import {HelpComponent} from "./help/help.component";
 import {SelectorStaticComponent} from "./selector-static/selector-static.component";
+import {SelectorDynamicEntryComponent} from "./selector-dynamic-entry/selector-dynamic-entry.component";
 
-const appRoutes: Routes = [{
-    path: 'selector-static-list',
-    component: SelectorStaticListComponent
-},
+const appRoutes: Routes = [
+    {
+        path: 'selector-static-list',
+        component: SelectorStaticListComponent
+    },
     {
         path: 'selector-static',
         component: SelectorStaticComponent
+    },
+    {
+        path: 'selector-dynamic-entry',
+        component: SelectorDynamicEntryComponent
     },
     {
         path: 'help',
